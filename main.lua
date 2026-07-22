db08d9fb015f5c02ec0524a439c188b4117470d0
Kaktus64 <134180215+Kaktus64@users.noreply.github.com>
2026-07-22 18:40:20 -0500

    super mario

---
+++ main.lua
@@ -0,0 +1,199 @@
+-- name: [CS] \\#0f386f\\Meta \\#51316c\\Knight
+-- description: KNOW MY POWER
+
+local TEXT_MOD_NAME = "[CS] Meta Knight"
+
+-- Stops mod from loading if Character Select isn't on
+if not _G.charSelectExists then
+    djui_popup_create("\\#ffffdc\\\n"..TEXT_MOD_NAME.."\nRequires the Character Select Mod\nto use as a Library!\n\nPlease turn on the Character Select Mod\nand Restart the Room!", 6)
+    return 0
+end
+
+local META_KNIGHT_ICON = get_texture_info("metak-icon")
+
+local E_MODEL_META_KNIGHT = smlua_model_util_get_id("metak_geo")
+
+--local META_KNIGHT_TUNE = audio_stream_load("metak-menu-theme.ogg")
+
+--local META_KNIGHT_ART = get_texture_info("graffiti_metak")
+
+
+local VOICETABLE_META_KNIGHT = {
+    [CHAR_SOUND_OKEY_DOKEY] = nil, -- Starting game
+	[CHAR_SOUND_LETS_A_GO] = 'mk_exhale.ogg', -- Starting level
+	[CHAR_SOUND_PUNCH_YAH] = 'mk_dah.ogg', -- Punch 1
+	[CHAR_SOUND_PUNCH_WAH] = 'mk_shoo.ogg', -- Punch 2
+	[CHAR_SOUND_PUNCH_HOO] = 'mk_hah2.ogg', -- Punch 3
+	[CHAR_SOUND_YAH_WAH_HOO] = nil, -- First/Second jump sounds
+	[CHAR_SOUND_HOOHOO] = nil, -- Third jump sound
+	[CHAR_SOUND_YAHOO_WAHA_YIPPEE] = nil, -- Triple jump sounds
+	[CHAR_SOUND_UH] = 'mk_ow2.ogg', -- Wall bonk
+	[CHAR_SOUND_UH2] = nil, -- Landing after long jump
+	[CHAR_SOUND_UH2_2] = nil, -- Same sound as UH2; jumping onto ledge
+	[CHAR_SOUND_HAHA] = nil, -- Landing triple jump
+	[CHAR_SOUND_YAHOO] = 'mk_hiyah.ogg', -- Long jump
+	[CHAR_SOUND_DOH] = 'mk_ow2.ogg', -- Long jump wall bonk
+	[CHAR_SOUND_WHOA] = 'mk_humph.ogg', -- Grabbing ledge
+	[CHAR_SOUND_EEUH] = 'mk_hmm.ogg', -- Climbing over ledge
+	[CHAR_SOUND_WAAAOOOW] = nil, -- Falling a long distance
+	[CHAR_SOUND_TWIRL_BOUNCE] = 'mk_hiyah.ogg', -- Bouncing off of a flower spring
+	[CHAR_SOUND_GROUND_POUND_WAH] = nil,
+	[CHAR_SOUND_HRMM] = 'mk_humph.ogg', -- Lifting something
+	[CHAR_SOUND_HERE_WE_GO] = nil, -- Star get
+	[CHAR_SOUND_SO_LONGA_BOWSER] = 'mk_knowmypower.ogg', -- Throwing Bowser
+--DAMAGE
+	[CHAR_SOUND_ATTACKED] = {'mk_hurt3.ogg', 'mk_hurt4.ogg', 'mk_hmm.ogg'}, -- Damaged
+	[CHAR_SOUND_PANTING] = nil, -- Low health
+	[CHAR_SOUND_ON_FIRE] = 'mk_hmm.ogg', -- Burned
+--SLEEP SOUNDS
+	[CHAR_SOUND_IMA_TIRED] = nil, -- Mario feeling tired
+	[CHAR_SOUND_YAWNING] = nil, -- Mario yawning before he sits down to sleep
+	[CHAR_SOUND_SNORING1] = nil, -- Snore Inhale
+	[CHAR_SOUND_SNORING2] = nil, -- Exhale
+	[CHAR_SOUND_SNORING3] = nil, -- Sleep talking / mumbling
+--COUGHING (USED IN THE GAS MAZE)
+	[CHAR_SOUND_COUGHING1] = nil, -- Cough take 1
+	[CHAR_SOUND_COUGHING2] = nil, -- Cough take 2
+	[CHAR_SOUND_COUGHING3] = nil, -- Cough take 3
+--DEATH
+	[CHAR_SOUND_DYING] = 'mk_hurt2.ogg', -- Dying from damage
+	[CHAR_SOUND_DROWNING] = 'mk_surface.ogg', -- Running out of air underwater
+    [CHAR_SOUND_MAMA_MIA] = 'mk_hurt_1.ogg', -- Booted out of level
+--EXTRAS
+    --[CHAR_SOUND_PRESS_START_TO_PLAY] = 'sm64_moneybags_jump.ogg'
+}
+
+
+
+local PALETTE_META_KNIGHT = {
+
+    [PANTS]  = "FFC400", -- GOLD BLADES
+    [SHIRT]  = "342E58", -- SHOULDER BLADES
+    [GLOVES] = "ffffff", -- GLOVES
+    [SHOES]  = "5E1376", -- SHOES
+    [HAIR]   = "675790", -- WINGS
+    [SKIN]   = "0D1753", -- BODY
+    [CAP]    = "C2C2C2", -- MASK
+	[EMBLEM] = "FFC800"  -- EYES
+}
+
+local PALETTE_MK_DARK = {
+
+    [PANTS]  = "c6ced2", -- GOLD BLADES
+    [SHIRT]  = "3f4854", -- SHOULDER BLADES
+    [GLOVES] = "ffffff", -- GLOVES
+    [SHOES]  = "650804", -- SHOES
+    [HAIR]   = "3a4346", -- WINGS
+    [SKIN]   = "555d68", -- BODY
+    [CAP]    = "C2C2C2", -- MASK
+	[EMBLEM] = "FFC800"  -- EYES
+}
+
+local PALETTE_MK_GALACTA = {
+
+    [PANTS]  = "565e64", -- GOLD BLADES
+    [SHIRT]  = "c5c9cc", -- SHOULDER BLADES
+    [GLOVES] = "ffffff", -- GLOVES
+    [SHOES]  = "b12a78", -- SHOES
+    [HAIR]   = "ffffff", -- WINGS
+    [SKIN]   = "ee46b3", -- BODY
+    [CAP]    = "fcfffe", -- MASK
+	[EMBLEM] = "ff0300"  -- EYES
+}
+
+local PALETTE_MK_MORPHO = {
+
+    [PANTS]  = "e80d09", -- GOLD BLADES
+    [SHIRT]  = "ffc500", -- SHOULDER BLADES
+    [GLOVES] = "ffc500", -- GLOVES
+    [SHOES]  = "ffc500", -- SHOES
+    [HAIR]   = "fb4e02", -- WINGS
+    [SKIN]   = "000000", -- BODY
+    [CAP]    = "e80d09", -- MASK
+	[EMBLEM] = "ffffff"  -- EYES
+}
+
+local PALETTE_MK_KIRBY = {
+
+    [PANTS]  = "b8124d", -- GOLD BLADES
+    [SHIRT]  = "f06775", -- SHOULDER BLADES
+    [GLOVES] = "f598b1", -- GLOVES
+    [SHOES]  = "ce1956", -- SHOES
+    [HAIR]   = "842f55", -- WINGS
+    [SKIN]   = "f598b1", -- BODY
+    [CAP]    = "f1727f", -- MASK
+	[EMBLEM] = "0057bc"  -- EYES
+}
+
+local PALETTE_MK_RETRO = {
+
+    [PANTS]  = "fcb30d", -- GOLD BLADES
+    [SHIRT]  = "fcb30d", -- SHOULDER BLADES
+    [GLOVES] = "ffffff", -- GLOVES
+    [SHOES]  = "5e54a0", -- SHOES
+    [HAIR]   = "f62413", -- WINGS
+    [SKIN]   = "000000", -- BODY
+    [CAP]    = "5e54a0", -- MASK
+	[EMBLEM] = "fcb30d"  -- EYES
+}
+
+
+--[[
+
+local HM_META_KNIGHT = {
+    label = {
+        left = get_texture_info("KakLeftHealth"),
+        right = get_texture_info("KakRightHealth"),
+    },
+    pie = {
+        [1] = get_texture_info("KakPie1"),
+        [2] = get_texture_info("KakPie2"),
+        [3] = get_texture_info("KakPie3"),
+        [4] = get_texture_info("KakPie4"),
+        [5] = get_texture_info("KakPie5"),
+        [6] = get_texture_info("KakPie6"),
+        [7] = get_texture_info("KakPie7"),
+        [8] = get_texture_info("KakPie8"),
+    }
+}
+
+local COURSE_META_KNIGHT = {
+    top = get_texture_info("KakCourse1"),
+    bottom = get_texture_info("KakCourse2"),
+}
+
+local CAPTABLE_META_KNIGHT = {
+    normal = smlua_model_util_get_id("kakcap_geo"),
+    wing = smlua_model_util_get_id("kakwingcap_geo"),
+    metal = smlua_model_util_get_id("kakmetalcap_geo"),
+    --metalWing = smlua_model_util_get_id("custom_model_cap_wing_geo")
+}
+
+--]]
+
+local CSloaded = false
+
+if _G.charSelectExists then
+    CT_META_KNIGHT = _G.charSelect.character_add("Meta Knight", {"know his power"}, "Kaktus64", {r = 81, g = 49, b = 108}, E_MODEL_META_KNIGHT, CT_MARIO, META_KNIGHT_ICON, 1.1)
+end
+
+local function on_character_select_load()
+
+    CSloaded = true
+
+    _G.charSelect.character_add_voice(E_MODEL_META_KNIGHT, VOICETABLE_META_KNIGHT)
+
+    _G.charSelect.character_add_palette_preset(E_MODEL_META_KNIGHT, PALETTE_META_KNIGHT, "Meta Knight")
+    _G.charSelect.character_add_palette_preset(E_MODEL_META_KNIGHT, PALETTE_MK_DARK, "Mirror World")
+    _G.charSelect.character_add_palette_preset(E_MODEL_META_KNIGHT, PALETTE_MK_GALACTA, "Sealed Away")
+    _G.charSelect.character_add_palette_preset(E_MODEL_META_KNIGHT, PALETTE_MK_MORPHO, "Butterfly")
+    _G.charSelect.character_add_palette_preset(E_MODEL_META_KNIGHT, PALETTE_MK_KIRBY, "Superstar Warrior")
+    _G.charSelect.character_add_palette_preset(E_MODEL_META_KNIGHT, PALETTE_MK_RETRO, "NES")
+
+end
+
+hook_event(HOOK_ON_MODS_LOADED, on_character_select_load)
+
+function is_metak()
+    return CT_META_KNIGHT == charSelect.character_get_current_number()
+end
---
+++ moveset.lua
@@ -0,0 +1,165 @@
+function is_metak()
+    return CT_META_KNIGHT == charSelect.character_get_current_number()
+end
+
+---@diagnostic disable: undefined-global
+if not _G.charSelectExists then return end
+
+local gStateExtras = {}
+for i = 0, MAX_PLAYERS - 1 do
+    gStateExtras[i] = {}
+    local m = gMarioStates[i]
+    local e = gStateExtras[i]
+    e.rotAngle = 0
+    e.charArg = 0
+    e.metaPoints = 0
+    e.metaQuickTimer = 0
+    e.metaFlyCount = 0
+end
+
+local function limit_angle(a)
+    return (a + 0x8000) % 0x10000 - 0x8000
+end
+
+local metaKFlyActions = {
+    [ACT_JUMP] = true,
+    [ACT_DOUBLE_JUMP] = true,
+    [ACT_TRIPLE_JUMP] = true,
+    [ACT_FREEFALL] = true,
+    [ACT_LONG_JUMP] = true,
+    [ACT_SIDE_FLIP] = true,
+    [ACT_BACKFLIP] = true,
+    [ACT_WALL_KICK_AIR] = true,
+    [ACT_TOP_OF_POLE_JUMP] = true,
+    [ACT_JUMP_KICK] = true,
+}
+
+ACT_METAK_FLY = allocate_mario_action(ACT_GROUP_AIRBORNE | ACT_FLAG_AIR | ACT_FLAG_MOVING)
+
+function act_metak_fly(m)
+ local e = gStateExtras[m.playerIndex]
+    local stepResult = common_air_action_step(m, ACT_FREEFALL_LAND, CHAR_ANIM_DOUBLE_JUMP_RISE, AIR_STEP_NONE)
+    m.faceAngle.y = m.intendedYaw - approach_s32(limit_angle(m.intendedYaw - m.faceAngle.y), 0, 0x300, 0x300)
+    if m.playerIndex == 0 then 
+    m.peakHeight = m.pos.y -- no fall sound
+    --m.vel.y = m.vel.y + 3
+    --SOUND_ACTION_SPIN
+        if m.controller.buttonPressed & A_BUTTON ~= 0 and e.metaFlyCount ~= 3 then
+            set_anim_to_frame(m, 0)
+            e.metaFlyCount = e.metaFlyCount + 1
+            play_sound(SOUND_ACTION_SPIN, m.marioObj.header.gfx.cameraToObject)
+            m.vel.y = 35
+            set_mario_particle_flags(m, PARTICLE_MIST_CIRCLE, 0)
+            set_mario_action(m, ACT_METAK_FLY, 0)
+        end
+    if stepResult == AIR_STEP_LANDED then
+        return set_mario_action(m, ACT_FREEFALL_LAND, 0)
+    end
+    if stepResult == AIR_STEP_HIT_WALL then
+        mario_bonk_reflection(m, 0)
+        return set_mario_action(m, ACT_METAK_FLY, 0)
+    end
+    if m.input & INPUT_Z_PRESSED ~= 0 then
+        return set_mario_action(m, ACT_GROUND_POUND, 0)
+    end
+    if m.input & INPUT_B_PRESSED ~= 0 then
+        m.forwardVel = 40
+        return set_mario_action(m, ACT_DIVE, 0)
+    end
+end
+end
+hook_mario_action(ACT_METAK_FLY, act_metak_fly)
+
+function metak_update(m)
+
+    local e = gStateExtras[m.playerIndex]
+
+    if m.controller.buttonPressed & X_BUTTON ~= 0 then
+        e.metaQuickTimer = 300
+    end
+
+    if e.metaQuickTimer > 0 then
+
+        e.metaQuickTimer = e.metaQuickTimer - 1
+
+        if m.action == ACT_WALKING and m.forwardVel > 45 and m.forwardVel < 55 then
+            m.forwardVel = m.forwardVel + 0.965
+        end
+        if m.action == ACT_WALKING and m.forwardVel > 35 and m.forwardVel < 40 then
+            m.forwardVel = m.forwardVel + 1.02
+        end
+        if m.action == ACT_WALKING and m.forwardVel > 32 and m.forwardVel < 32.1 then
+            m.forwardVel = 40
+        end
+        if m.action == ACT_WALKING and m.forwardVel > 35 and m.forwardVel < 60 then
+            m.forwardVel = m.forwardVel + 1.02
+        end
+        if m.action == ACT_WALKING and m.forwardVel > 32 and m.forwardVel < 33 then
+            m.forwardVel = 40
+            set_mario_particle_flags(m, PARTICLE_VERTICAL_STAR, 0)
+            play_mario_jump_sound(m)
+            play_sound(SOUND_GENERAL_COIN_SPURT_EU, m.marioObj.header.gfx.cameraToObject)
+        end
+        if m.action == ACT_WALKING and m.forwardVel > 35 then
+            m.marioBodyState.torsoAngle.x = 0
+            --smlua_anim_util_set_animation(m.marioObj, "mario_fast_run")
+            set_mario_particle_flags(m, PARTICLE_DUST, 0)
+            obj_get_temp_spawn_particles_info(E_MODEL_SMOKE)
+        end
+
+    end
+
+    if metaKFlyActions[m.action] and m.vel.y < 0 and m.input & INPUT_A_PRESSED ~= 0 and e.metaFlyCount ~= 3 then
+        play_sound(SOUND_ACTION_SPIN, m.marioObj.header.gfx.cameraToObject)
+        set_mario_action(m, ACT_METAK_FLY, 0)
+        set_mario_particle_flags(m, PARTICLE_MIST_CIRCLE, 0)
+        m.vel.y = 35
+        e.metaFlyCount = e.metaFlyCount + 1
+    end
+
+    if m.pos.y == m.floorHeight then
+        e.metaFlyCount = 0
+    end
+
+    if m.action == ACT_WALKING then
+        m.marioBodyState.torsoAngle.x = 0
+        m.marioBodyState.torsoAngle.z = 0
+    end
+
+end
+
+function metak_set_action(m)
+
+end
+
+function metak_before_set_action(m, inc)
+
+end
+
+function metak_hud(m)
+
+    local m = gMarioStates[0]
+
+    local e = gStateExtras[m.playerIndex]
+
+    djui_hud_set_resolution(RESOLUTION_N64)
+
+    djui_hud_set_font(FONT_SPECIAL)
+
+    djui_hud_set_color(255, 0, 0, 255)
+
+    local metaPoints = string.format("MP: %.0f", e.metaPoints)
+    local metaQuickTimer = string.format("MQ: %.0f", e.metaQuickTimer)
+    local metaFlyCount = string.format("MF: %.0f", e.metaFlyCount)
+
+    djui_hud_print_text(metaPoints, 125, 220, 0.5)
+    djui_hud_print_text(metaQuickTimer, 125, 200, 0.5)
+    djui_hud_print_text(metaFlyCount, 125, 180, 0.5)
+
+
+end
+
+_G.charSelect.character_hook_moveset(CT_META_KNIGHT, HOOK_MARIO_UPDATE, metak_update)
+_G.charSelect.character_hook_moveset(CT_META_KNIGHT, HOOK_ON_SET_MARIO_ACTION, metak_set_action)
+_G.charSelect.character_hook_moveset(CT_META_KNIGHT, HOOK_BEFORE_SET_MARIO_ACTION, metak_before_set_action)
+_G.charSelect.character_hook_moveset(CT_META_KNIGHT, HOOK_ON_HUD_RENDER, metak_hud)
